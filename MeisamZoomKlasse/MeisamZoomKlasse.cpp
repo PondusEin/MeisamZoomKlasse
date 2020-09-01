e110324601a3708652dc06c10f7646c4f3404bf2
PondusEin <PondusEin@users.noreply.github.com>
2020-09-01 11:55:42 +0200

    testing out new code

--- MeisamZoomKlasse/MeisamZoomKlasse.cpp
+++ MeisamZoomKlasse/MeisamZoomKlasse.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
 
+//using namespace std;
+int age;
+
 int main()
 {
-    std::cout << "Hello World!\n";
-    std::cout << "Goodbye World!";
+	
+	std::cout << "Enter your age: ";
+	std::cin >> age;
+	std::cout << "Your age is: " << age << std::endl; //"\n"; == std::endl;  \t == Tab for mer klart kode
+	std::cout << "You are young";
+	/*std::cout << "s";
+	*/
 }
