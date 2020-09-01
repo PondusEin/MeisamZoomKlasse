776e2b3ced9ddd43370de074ef5fc48dc6b27fe9
PondusEin <PondusEin@users.noreply.github.com>
2020-09-01 12:06:19 +0200

    Added text and different things to test

--- MeisamZoomKlasse/MeisamZoomKlasse.cpp
+++ MeisamZoomKlasse/MeisamZoomKlasse.cpp
@@ -1,11 +1,25 @@
 #include <iostream>
+#include <string>
 
+//global
 //using namespace std;
-int age;
+int age = 26;
+bool sw = false; // Writing Switch with big S, as it is case sensetive
+char Initial = 'm';
+std::string name = "Erik";
+double pi = 3.14;
 
 int main()
 {
-	
+	//Local
+	/*//using namespace std; 
+	int age = 0;
+	bool sw = false; // Writing Switch with big S, as it is case sensetive
+	char Initial = 'm';
+	std::string name = "Erik";
+	double pi = 3.14;
+	*/
+	std::cout << sw;
 	std::cout << "Enter your age: ";
 	std::cin >> age;
 	std::cout << "Your age is: " << age << std::endl; //"\n"; == std::endl;  \t == Tab for mer klart kode
@@ -13,3 +27,4 @@ int main()
 	/*std::cout << "s";
 	*/
 }
+
